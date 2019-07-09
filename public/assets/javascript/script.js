3ce8e3d7b6d714009acb93a47b6971234477e4a0
shualton <shualton@gmail.com>
2019-07-08 19:40:35 -0700

    updated ajax call in script

---
+++ public/assets/javascript/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".devour-form").on("submit", function(event) {
+        event.preventDefault();
+        var burger_id = $(this).children(".burger_id").val();
+        $.ajax({
+          method: "PUT",
+          url: "/burgers/" + burger_id
+        }).then(function(res) {
+          location.reload();
+        });
+    
+      });
+})
