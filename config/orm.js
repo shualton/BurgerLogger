262a0b10b09c0dfdaa95e370b96883e8df1c4a02
shualton <shualton@gmail.com>
2019-07-08 21:20:03 -0700

    completed orm

--- config/orm.js
+++ config/orm.js
@@ -16,4 +16,44 @@ function print(num) {
     return arr.toString();
   }
 
+  var orm = {
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += print(vals.length);
+      queryString += ") ";
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    update: function(table, obj, condition, cb) {
+      var queryString = "UPDATE " + table;
+      queryString += " SET ";
+      queryString += objSql(obj);
+      queryString += " WHERE ";
+      queryString += condition;
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    }
+  };
   
+  module.exports = orm;
